6fe4293725d297c40094e169763186daec1cb7a1
Pavel <Pavel.Vlasov@nasdanika.org>
2024-04-27 09:35:41 -0400

    Builds distribution

---
+++ src/test/java/org/nasdanika/launcher/tests/BuildDistributionIT.java
@@ -0,0 +1,27 @@
+package org.nasdanika.launcher.tests;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+import org.nasdanika.cli.LauncherCommand;
+
+import picocli.CommandLine;
+
+public class BuildDistributionIT {
+		
+	@Test
+	public void generateLauncher() throws IOException {
+		CommandLine launcherCommandLine = new CommandLine(new LauncherCommand());
+		launcherCommandLine.execute(
+				"-b", "target/dist", 
+				"-o", "nsd.bat");
+		
+		launcherCommandLine.execute(
+				"-b", "target/dist", 
+				"-o", "nsd",
+				"-p", ":",
+				"-a", "$@");		
+		
+	}
+
+}
