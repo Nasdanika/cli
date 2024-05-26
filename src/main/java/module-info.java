d6e3471433efff117197ea762571028334ddcfb8
Pavel <Pavel.Vlasov@nasdanika.org>
2024-05-26 12:56:04 -0400

    Rules WIP

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module org.nasdanika.launcher {
 	
 	requires transitive org.nasdanika.cli;
 	requires transitive org.nasdanika.html.model.app.gen.cli;
+	requires transitive org.nasdanika.models.rules.cli;
 		
 	requires org.nasdanika.models.echarts.graph;
 		
