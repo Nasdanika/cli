0db79b3f4bb748f838a0fb8bbe5ec2ce41066953
Pavel <Pavel.Vlasov@nasdanika.org>
2024-06-05 07:55:33 -0400

    Shell command

--- src/main/java/org/nasdanika/launcher/Launcher.java
+++ src/main/java/org/nasdanika/launcher/Launcher.java
@@ -8,7 +8,9 @@ import org.nasdanika.capability.CapabilityLoader;
 import org.nasdanika.capability.CapabilityProvider;
 import org.nasdanika.capability.ServiceCapabilityFactory;
 import org.nasdanika.capability.ServiceCapabilityFactory.Requirement;
+import org.nasdanika.cli.ShellCommand;
 import org.nasdanika.cli.SubCommandRequirement;
+import org.nasdanika.common.Closeable;
 import org.nasdanika.common.NullProgressMonitor;
 import org.nasdanika.common.ProgressMonitor;
 
@@ -30,7 +32,15 @@ public class Launcher {
 		
 		// Executing the first one
 		for (CommandLine rootCommand: rootCommands) {	
-			int exitCode = rootCommand.execute(args);
+			rootCommand.addSubcommand(new ShellCommand(rootCommand));
+			int exitCode;
+			try {
+				exitCode = rootCommand.execute(args);
+			} finally {
+				if (rootCommand instanceof Closeable) {
+					((Closeable) rootCommand).close(progressMonitor.split("Closing root command", 1));
+				}
+			}
 			System.exit(exitCode);
 		}
 		
