db65789fc491d5f73a6a0c75d14c0b90096f9970
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2024-11-14 05:58:43 -0500

    2024.11.0

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,7 @@ module org.nasdanika.launcher {
 	exports org.nasdanika.launcher;
 	
 	requires transitive org.nasdanika.cli;
-	requires transitive org.nasdanika.html.model.app.gen.cli;
+	requires transitive org.nasdanika.models.app.cli;
 	requires transitive org.nasdanika.models.rules.cli;
 	requires transitive org.nasdanika.models.architecture;
 	requires transitive org.nasdanika.models.ecore.cli;
