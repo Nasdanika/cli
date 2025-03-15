fbad2535c29eb9b7b8df5ebc244fff5d8c5ed179
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2025-03-15 11:27:58 -0400

    CrewAI

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,10 +3,6 @@ module org.nasdanika.launcher {
 	exports org.nasdanika.launcher;
 	
 	requires transitive org.nasdanika.cli;
-	requires transitive org.nasdanika.models.app.cli;
-	requires transitive org.nasdanika.models.rules.cli;
-	requires transitive org.nasdanika.models.architecture;
-	requires transitive org.nasdanika.models.ecore.cli;
 		
 	requires org.nasdanika.models.echarts.graph;
 	requires java.sql;
